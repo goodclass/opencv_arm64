18e0f6055046b94bec6095c78af552e13846d7d2
goodclass <goodclass@126.com>
2019-08-08 12:21:47 +0800

    arm64
    arm64

---
+++ cv2_source/pyopencv_custom_headers.h
@@ -0,0 +1,11 @@
+//user-defined headers
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/core/misc/python/pyopencv_async.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/core/misc/python/pyopencv_cuda.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/core/misc/python/pyopencv_umat.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/flann/misc/python/pyopencv_flann.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/ml/misc/python/pyopencv_ml.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/dnn/misc/python/pyopencv_dnn.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/features2d/misc/python/pyopencv_features2d.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/videoio/misc/python/pyopencv_videoio.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/objdetect/misc/python/pyopencv_objdetect.hpp"
+#include "/Users/goodclass/Downloads/PythonSite/opencv-4.1.1/modules/stitching/misc/python/pyopencv_stitching.hpp"
